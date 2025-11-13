bcfd099349326c4d9c194cbc9a397fac0770cb0d
Benjamin <33324686+benjamin-larsen@users.noreply.github.com>
2025-11-13 12:19:09 +0100

    Update base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import NoctesPlugin from 'noctes.jsx-vite-plugin'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [NoctesPlugin()],
-  base: "./",
+  base: "/",
   build: {
     outDir: "./dist"
   }
